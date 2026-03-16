2c7d83def481a06764bfdfd79f1950f641ed3247
Felipe Tomas Pagani <66705993+the-other-flaneur@users.noreply.github.com>
2026-03-16 16:52:34 -0300

    solved: 01-isUnique

--- technical-fundamentals/coding/problems/01_isUnique.ts
+++ technical-fundamentals/coding/problems/01_isUnique.ts
@@ -3,4 +3,15 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
 
-export default function isUnique(str: string): boolean {}
+export default function isUnique(str: string): boolean {
+	
+	let result: boolean = true;
+
+	for (let i = 0; i < str.length; i++) {
+		for (let j = i + 1; j < str.length; j++) {
+			if (str[i] === str[j]) result = false;
+		}
+	}
+
+	return result;
+}
