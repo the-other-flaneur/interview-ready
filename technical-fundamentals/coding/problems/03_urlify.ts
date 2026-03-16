dae113520e899ed8e5c1ed8391f0fa0f255a1307
Felipe Tomas Pagani <66705993+the-other-flaneur@users.noreply.github.com>
2026-03-16 17:26:03 -0300

    solved: 03_URLify

--- technical-fundamentals/coding/problems/03_urlify.ts
+++ technical-fundamentals/coding/problems/03_urlify.ts
@@ -5,5 +5,16 @@
 // and that you are given the "true" length of the string.
 
 export default function URLify (s1 : string): string {
+	
+	let result = '';
 
+	for (let i = 0; i < s1.length; i++) {
+		if (s1[i] == ' ') {
+			result = result + '%20';
+		} else {
+			result = result + s1[i];
+		}
+	}
+
+	return result;
 }
