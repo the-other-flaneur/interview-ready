84900d12a0c66316b648822ccc4240609d7445e8
Felipe Tomas Pagani <66705993+the-other-flaneur@users.noreply.github.com>
2026-03-18 13:18:49 -0300

    solved: 09_stringRotation.ts

--- technical-fundamentals/coding/problems/09_stringRotation.ts
+++ technical-fundamentals/coding/problems/09_stringRotation.ts
@@ -6,4 +6,9 @@ import { isSubstring } from "./__utils__/strings"
 // Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring.
 // [e.g., "waterbottle" is a rotation of 'erbottlewat")
 
-export default function stringRotation(s1: string, s2: string): boolean {}
+export default function stringRotation(s1: string, s2: string): boolean {
+	if (s1.length !== s2.length || s1.length === 0) return false;
+  
+  	const doubled = s1 + s1;
+  	return isSubstring(doubled, s2);
+}
